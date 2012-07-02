6da34c1f51f21715981b88e5d353303405272fad
Mariano Guerra <luismarianoguerra@gmail.com>
2012-07-02 12:09:19 +0200

    fix lambda tests

--- test/squim.modules.test.js
+++ test/squim.modules.test.js
@@ -315,8 +315,11 @@
             Q.equal(result.operative.formals.value, 'x');
             Q.equal(result.operative.expr.value, 'x');
 
-            result = Squim.run('(apply ($lambda x x) 2)');
+            result = Squim.run('(($lambda (x) x) 2)');
             Q.equal(result.value, 2);
+
+            result = Squim.run('(($lambda x x) 2)');
+            Q.equal(result.left.value, 2);
         });
     };
 
