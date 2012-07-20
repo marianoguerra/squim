581096e2c7ed03e732575e78ac096bf3d578c020
Mariano Guerra <luismarianoguerra@gmail.com>
2012-07-20 12:09:39 +0200

    if Obj is in function place then resolve args as attr names
    args should be symbols, the first is resolved and the result gets called
    with the remaining arguments.
    
    in this case if the rest is also an object it will keep resolving them
    
    if the attribute resolves to an object then it will call that function

--- src/squim.types.js
+++ src/squim.types.js
@@ -424,6 +424,41 @@
         return new Obj(squimattrs);
     };
 
+    Obj.prototype.apply = function (thisArg, funargs) {
+        var
+            thisobj = this,
+            args = funargs[0],
+            cc = funargs[1],
+            dynamicEnv = cc.env,
+            value;
+
+        if (args instanceof Pair.Nil) {
+            // with no args it evaluates to itself
+            return cc.resolve(this);
+        } else if (args.left instanceof Symbol) {
+            // with one or more it evals the left one and tries to find the
+            // symbol in the object and returns that attr
+            value = thisobj.attrs[args.left.value];
+
+            if (value === undefined) {
+                // TODO: throw an error?
+                return cc.resolve(obj.inert);
+            } else if (args.right instanceof Pair.Nil) {
+                // if no more args, resolve to the attr
+                return cc.resolve(value);
+            } else {
+                // if more args apply the attribute to the rest
+                // of the arguments
+                return value.apply(thisArg, [args.right, cc]);
+            }
+        } else {
+            return Error.BadMatch(
+                "expected symbl in argument list",
+                {args: args, got: args.left});
+        }
+    };
+
+
     function Env(bindings, parents, inmutable) {
 
         if (bindings === undefined) {
--- test/squim.types.test.js
+++ test/squim.types.test.js
@@ -108,6 +108,25 @@
 
         });
 
+        Q.test("Obj.apply", function () {
+            var jsobj = {name: "mariano", age: 27, tags: ["foo", "bar"], foo: {bar: 1, baz: "asd", argh: {lala: "hi"}}},
+                obj = Types.Obj.fromJsObject(jsobj),
+                env = new Types.Env({"obj": obj}, [Types.Env.makeGround()]);
+
+            Q.equal(Squim.run("(obj)", env), obj, "calling obj without args evaluates to itself");
+            Q.equal(Squim.run("(eq? (obj) obj)", env), Types.t, "eq? works on obj");
+            Q.equal(Squim.run("(obj name)", env), obj.attrs.name, "calling obj with one symbol returns that attribute");
+            Q.equal(Squim.run('(equal? (obj name) "mariano")', env), Types.t, "calling obj with one symbol returns that attribute");
+            Q.equal(Squim.run("(obj foo bar)", env), obj.attrs.foo.attrs.bar, "calling obj with symbols returns that attribute");
+            Q.equal(Squim.run('(equal? (obj foo bar) 1)', env), Types.t, "calling obj with one symbol returns that attribute");
+            Q.equal(Squim.run('(equal? (obj foo argh lala) "hi")', env), Types.t, "calling obj with symbols returns that attribute");
+            Q.equal(Squim.run("(obj foo argh lala)", env), obj.attrs.foo.attrs.argh.attrs.lala, "calling with symbols returns that attribute");
+
+            // XXX: this may change to throw an error later
+            Q.equal(Squim.run('(obj inexistent)', env), Types.inert, "calling obj with unknown symbol returns inert");
+            Q.equal(Squim.run('(equal? (obj inexistent) #inert)', env), Types.t, "calling obj with unknown symbol returns inert");
+        });
+
         Q.test("squimify", function () {
             function check(value, expected) {
                 Q.equal(Types.squimify(value).toString(), expected);
